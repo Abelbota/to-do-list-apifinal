ae8a707f876c7a4dea9f447642eccec3f8d9bb73
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-03-11 21:07:21 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/service/ToDoItemService.java
@@ -0,0 +1,16 @@
+package org.fasttrackit.service;
+
+import org.fasttrackit.persistence.ToDoItemRepository;
+import org.fasttrackit.transfer.SaveToDoItemRequest;
+
+import java.sql.SQLException;
+
+public class ToDoItemService {
+
+    private ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+
+    public void createToDoItem(SaveToDoItemRequest request) throws SQLException {
+        System.out.println("Creating to do item: " + request);
+        toDoItemRepository.createToDoItem(request);
+    }
+}
